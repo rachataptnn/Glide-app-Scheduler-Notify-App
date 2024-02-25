b13ba0d0e6afbcb34510d7e4c16251c3666f91fd
rachata.p <133649727+rachataptnn@users.noreply.github.com>
2024-02-25 13:50:22 +0700

    update code - add more comment for easier reading

--- notification.js
+++ notification.js
@@ -1,26 +1,32 @@
 function sendDailyNotifications() {
+  // Retrieve user and notification data from the sheet
   var data = getDataFromSheet();
   var users = data.users;
   var notifications = data.notifications;
 
+  // Iterate through each notification entry
   for (var i = 1; i < notifications.length; i++) {
     var notificationRow = notifications[i];
     var notificationTime = notificationRow[1];
     var notificationMessage = notificationRow[2];
      
+    // Check if it's time for this notification
     if (isTimeForNotification(notificationTime)) {
       var userId = notificationRow[0];
+
+      // Find the user corresponding to the notification
       var foundUser = users.find(function(user) {
         return user[0] == userId;
       });
-      Logger.log("found user: ", foundUser)
       
+      // If user is found, send the notification
       if (foundUser) {
         var userName = foundUser[1];
         var lineAuthToken = foundUser[2];
         sendNotificationToUser(lineAuthToken, notificationMessage, userName);
       } else {
-        Logger.log("User ID:" + userId + ", Name : " + userName + " is not found in sheet Users")
+        // Log an error if user is not found
+        Logger.log("User ID: " + userId + " is not found in the Users sheet.");
       }
     }
   }
